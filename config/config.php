2c363e58ab1145868897278ddd97e0eead51fae2
rockleee <aanabdulrohman3@gmail.com>
2023-05-23 22:45:29 +0700

    menambah fitur add di curd

--- config/config.php
+++ config/config.php
@@ -4,5 +4,5 @@ $databaseName = 'rd_db';
 $databaseUsername = 'root';
 $databasePassword = '';
 
-$mysqli = mysqli_connect($databaseHost, $databaseName, $databaseUsername, $databasePassword);
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
     // menghubungkan database via file config.php
-    include_once("config.php");
+    include_once("config/config.php");
 
     // fetch all user data form database
     $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
@@ -17,26 +17,22 @@
     <title>Aan abdul rohman</title>
   </head>
   <body>
-    <a href="tambah.php">Tambah baru</a><br><br>
+    <a href="config/add.php">Tambah baru</a><br><br>
 <table width="80%" border="1">
   <tr>
     <th>Nama</th>
-    <th>No hp</th>
-    <th>Email</th>
+    <th>email</th>
     <th>Update</th>
   </tr>
 
   <?php 
   while($user_data = mysqli_fetch_array($result)){
     echo "<tr>";
-    echo "<td>".$user_data['nama']."</td>";
-    echo "<td>".$user_data['hp']."</td>";
+    echo "<td>".$user_data['name']."</td>";
     echo "<td>".$user_data['email']."</td>";
-    echo "<td>".$user_data['update']."</td>";
     echo "<td><a href='edit.php?id=$user_data[id]'>Edit</a> | <a href='delete.php?id=$user_data[id]'>Delete</a></td></tr>";
   }
   ?>
-
 </table>
   </body>
 </html>
